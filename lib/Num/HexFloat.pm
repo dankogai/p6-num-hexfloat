5d956037f27ee38f7904e61358a40a7c16abfae9
Dan Kogai <dankogai+github@gmail.com>
2016-01-08 13:15:34 +0900

    add proto

--- lib/Num/HexFloat.pm
+++ lib/Num/HexFloat.pm
@@ -10,19 +10,22 @@ our $RE_HEXFLOAT is export = rx:i{
     $<exp>   = [<[+-]>? <[0..9]>+]
 };
 # should've been multi but it didn't work :(
-sub from-hexfloat($arg) returns Num is export {
-    my Match $m = $arg ~~ Match
-    ?? $arg !!  $arg.match($RE_HEXFLOAT);
+proto from-hexfloat($arg) returns Num is export { * }
+multi sub from-hexfloat(Match $m) returns Num {
     return NaN unless $m;
     my $sign = $m<sign> eq '-' ?? -1e0 !! 1e0;
     my $mantissa = :16( $m<int> ~ $m<fract> );
     my $exponent = $m<exp>.Num - 4*$m<fract>.chars;
     return $sign * $mantissa * 2e0 ** $exponent;
 }
-multi sub to-hexfloat(Numeric $num) returns Str is export {
-    to-hexfloat($num.Num);
+multi sub from-hexfloat(Str $s) returns Num {
+    from-hexfloat($s.match($RE_HEXFLOAT));
 }
-multi sub to-hexfloat(Num $num) returns Str is export {
+proto sub to-hexfloat($num) returns Str is export { * }
+multi sub to-hexfloat($num) returns Str {
+    to-hexfloat($num.Num)
+}
+multi sub to-hexfloat(Num $num) returns Str {
     my $s = $num < 0 ?? '-' !! '';
     my $a = $num.abs;
     my $p = 0;
